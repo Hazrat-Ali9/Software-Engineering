3927719fb381aeba964d6735ba67630379ea8081
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-12-04 10:25:38 +0600

    promise :white_check_mark:

--- core-js/promise.js
+++ core-js/promise.js
@@ -1,19 +1,4 @@
-// const myPromise = new Promise(function (resolve, reject) {
-//     let sum = 0;
-//     for (let i = 0; i < 100000000000; i++) {
-//         sum += i;
-//     }
-//     resolve(sum);
-// });
-
-// myPromise
-//     .then((res) => {
-//         console.log("result:", res);
-//     })
-//     .catch((e) => {
-//         console.log("error:", e);
-//     });
-
+// Promise 
 let sum = 0;
 for (let i = 0; i < 100000000000; i++) {
     sum += i;
